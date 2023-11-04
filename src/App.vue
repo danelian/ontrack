23e242f18a17349503e23cfa4253fd8ae337a1cb
danelian <141317269+danelian@users.noreply.github.com>
2023-11-05 01:46:34 +0300

    03. Nav template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -18,5 +19,32 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
+
+  <main class="flex flex-col flex-grow">
+
+  </main>
+
+  <nav class="sticky bottom-0 z-10 bg-white text-xl">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a href="#timeline" class="flex flex-col items-center p-2 text-xs capitalize text-center">
+          <ClockIcon class="w-6 h-6" />
+          Timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#activities" class="flex flex-col items-center p-2 text-xs capitalize text-center">
+          <ListBulletIcon class="w-6 h-6" />
+          Activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#progress" class="flex flex-col items-center p-2 text-xs capitalize text-center">
+          <ChartBarIcon class="w-6 h-6" />
+          Progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 
 </template>
