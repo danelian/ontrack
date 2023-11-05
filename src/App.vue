5906c9c87d0e5cd652dbe109db5b95b2202fda18
danelian <141317269+danelian@users.noreply.github.com>
2023-11-05 22:43:36 +0300

    16. Select box

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
-import { normalizePageHash } from './functions'
+import { normalizePageHash, generateTimelineItems } from './functions'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
@@ -9,6 +9,8 @@ import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
 
 const currentPage = ref(normalizePageHash())
+
+const timelineItems = generateTimelineItems()
 
 function goTo(page) {
   currentPage.value = page
@@ -22,7 +24,7 @@ function goTo(page) {
   />
 
   <main class="flex flex-col flex-grow">
-    <TheTimeline v-show="currentPage == PAGE_TIMELINE" />
+    <TheTimeline v-show="currentPage == PAGE_TIMELINE" :timeline-items="timelineItems" />
     <TheActivities v-show="currentPage == PAGE_ACTIVITIES" />
     <TheProgress v-show="currentPage == PAGE_PROGRESS" />
   </main>
