ac67356f0f14706b59a68cc326c13038c64d7e24
danelian <141317269+danelian@users.noreply.github.com>
2023-11-07 00:15:22 +0300

    31. Generate activity select options

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
-import { normalizePageHash, generateTimelineItems } from './functions'
+import { normalizePageHash, generateTimelineItems, generateActivitySelectOptions } from './functions'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
@@ -14,6 +14,8 @@ const timelineItems = generateTimelineItems()
 
 const activities = ['Coding', 'Reading', 'Training']
 
+const activitySelectOptions = generateActivitySelectOptions(activities);
+
 function goTo(page) {
   currentPage.value = page
 }
@@ -23,7 +25,10 @@ function goTo(page) {
   <TheHeader @navigate="goTo($event)" />
 
   <main class="flex flex-col flex-grow">
-    <TheTimeline v-show="currentPage == PAGE_TIMELINE" :timeline-items="timelineItems" />
+    <TheTimeline 
+      v-show="currentPage == PAGE_TIMELINE" 
+      :timeline-items="timelineItems" :activity-select-options="activitySelectOptions" 
+    />
     <TheActivities v-show="currentPage == PAGE_ACTIVITIES" :activities="activities" />
     <TheProgress v-show="currentPage == PAGE_PROGRESS" />
   </main>
