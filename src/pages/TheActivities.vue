db7b4a55b8fdee5d657d141947868ec0dec946a5
danelian <141317269+danelian@users.noreply.github.com>
2023-11-06 05:13:39 +0300

    28. Activity validators

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,13 +1,24 @@
 <script setup>
+import { validateActivities } from '../validators'
 import ActivityItem from '../components/ActivityItem.vue'
 
-const activities = ['Coding', 'Reading', 'Training']
+defineProps({
+  activities: {
+    required: true,
+    type: Array,
+    validator: validateActivities
+  }
+})
 </script>
 
 <template>
   <div>
     <ul class="divide-y">
-      <ActivityItem v-for="activity in activities" :key="activity" :activity="activity" />
+      <ActivityItem 
+        v-for="activity in activities"
+        :key="activity" 
+        :activity="activity"
+      />
     </ul>
   </div>
 </template>
