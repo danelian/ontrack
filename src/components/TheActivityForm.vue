e9eeea55bb1120ba7feb70b670c47f1de2e425e4
danelian <141317269+danelian@users.noreply.github.com>
2023-11-07 07:42:52 +0300

    37. nextTick utility

--- src/components/TheActivityForm.vue
+++ src/components/TheActivityForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 import { PlusIcon } from '@heroicons/vue/24/outline';
 import { isActivityValid } from '../validators'
 import BaseButton from './BaseButton.vue'
@@ -10,10 +10,19 @@ const emit = defineEmits({
 
 const activity = ref('')
 
-function submit() {
+async function submit() {
   emit('submit', activity.value)
   // очищаю поле ввода после отправки
   activity.value = ''
+  // инструкция по прокрутке страницы вниз,
+  // к созданному элементу
+  // nextTick().then(() => {
+  //   window.scrollTo(0, document.body.scrollHeight)
+  // })
+
+  await newsTick()
+
+  window.scrollTo(0, document.body.scrollHeight)
 }
 </script>
 
