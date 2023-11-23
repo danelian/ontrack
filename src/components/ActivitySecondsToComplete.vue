cca7cb8d8598666026682aa00ce992c6a0d9ce1f
danelian <141317269+danelian@users.noreply.github.com>
2023-11-23 21:35:53 +0300

    60. Update time of ActivitySecondsToComplete component

--- src/components/ActivitySecondsToComplete.vue
+++ src/components/ActivitySecondsToComplete.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { computed } from 'vue'
 import { formatSeconds, getTotalActivitySeconds } from '../functions'
 import { isActivityValid, validateTimelineItems } from '../validators'
 
@@ -15,15 +16,23 @@ const props = defineProps({
   }
 })
 
-const seconds = formatSeconds(
+const classes = computed(() => `flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600 ${colorClasses}`)
+
+const colorClasses = computed(() =>
+  secondsDiff.value < 0 ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600'
+)
+
+const seconds = computed(() => `${sign.value}${formatSeconds(secondsDiff.value)}`)
+
+const sign = computed(() => secondsDiff.value >= 0 ? '+' : '-')
+
+const secondsDiff = computed(() => 
   getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete
 )
 </script>
 
 <template>
-  <div
-    class="flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600"
-  >
+  <div :class="classes">
     {{ seconds }}
   </div>
 </template>
