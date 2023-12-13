419894f645e28cefda93731ee9d7a3d54cfb8449
danelian <141317269+danelian@users.noreply.github.com>
2023-12-14 01:29:22 +0300

    64. Inject activities and activity select options into TimelineItem ...
    component

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,9 @@ function setActivitySecondsToComplete(activity, secondsToComplete) {
 }
 
 provide('updateTimelineItemActivitySeconds', updateTimelineItemActivitySeconds)
+provide('activitySelectOptions', activitySelectOptions.value)
 provide('timelineItems', timelineItems.value)
+provide('activities', activities.value)
 
 </script>
 
@@ -75,8 +77,6 @@ provide('timelineItems', timelineItems.value)
     <TheTimeline
       v-show="currentPage == PAGE_TIMELINE"
       :timeline-items="timelineItems"
-      :activities="activities"
-      :activity-select-options="activitySelectOptions"
       :current-page="currentPage"
       ref="timeline"
       @set-timeline-item-activity="setTimelineItemActivity"
