3f37472fbf30db4df85d0e8f74c4f34654475999
danelian <141317269+danelian@users.noreply.github.com>
2023-12-27 00:34:43 +0400

    106. Simplify stopwatch logic

--- src/composables/stopwatch.js
+++ src/composables/stopwatch.js
@@ -1,17 +1,14 @@
 import { ref } from 'vue'
 import { MILLISECONDS_IN_SECOND } from '../constants'
 
-export function useStopwatch(initialSeconds, handleSecondsChange) {
+export function useStopwatch(initialSeconds) {
   const seconds = ref(initialSeconds)
   const isRunning = ref(false)
   const temp = 120
   
   function start() {
     isRunning.value = setInterval(() => {
-
       seconds.value += temp
-
-      handleSecondsChange()
     }, MILLISECONDS_IN_SECOND)
   }
   
@@ -24,8 +21,6 @@ export function useStopwatch(initialSeconds, handleSecondsChange) {
     stop()
 
     seconds.value = 0
-
-    handleSecondsChange()
   }
 
   return {
