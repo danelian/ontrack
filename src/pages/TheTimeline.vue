b5b389c00d1e315898e091b938427c1980124977
danelian <141317269+danelian@users.noreply.github.com>
2023-12-28 22:47:58 +0400

    123. Start timeline item timer globally

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { onActivated, onDeactivated } from 'vue'
-import { timelineItems, timelineItemRefs, scrollToCurrentHour } from '../timeline-items'
+import { timelineItems, timelineItemRefs, scrollToCurrentHour, stopTimelineItemTimer } from '../timeline-items'
 import { startTimer, stopTimer } from '../time'
 import TheTimelineIndicator from '../components/TheTimelineIndicator.vue'
 import TimelineItem from '../components/TimelineItem.vue'
+
+stopTimelineItemTimer()
 
 onActivated(() => {
   scrollToCurrentHour()
